c259cbeb0177bec860dd0924f06f5e5693c5a0d6
akshikasharma1011 <119088503+akshikasharma1011@users.noreply.github.com>
2024-07-14 21:29:24 +0530

    Lecture 19.cpp

---
+++ Lecture 19.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=5;
+	float b=6;
+	long long c=6;
+	long long int d=7;
+	cout<<"The size of int is "<<sizeof(a)<<"The size of float is "<<sizeof(b)<<"the size of long long  is "<<sizeof(c);
+	cout<<"The size of long long int "<<sizeof(d);
+	return 0;
+}
