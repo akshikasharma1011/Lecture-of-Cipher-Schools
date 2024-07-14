75ce75a048793bbe49a5cb994d06a3af9abc5d26
akshikasharma1011 <119088503+akshikasharma1011@users.noreply.github.com>
2024-07-14 21:30:29 +0530

    Lecture 20.cpp

---
+++ Lecture 20.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=5;
+	float b=6;
+	long long c=6;
+	cout<<"The address of int is "<<&(a)<<"The address of float is "<<&(b)<<"the address of long long  is "<<&(c);
+	return 0;
+}
