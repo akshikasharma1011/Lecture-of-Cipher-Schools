b75961f585c465c0445419106aa584f5b8fea185
akshikasharma1011 <119088503+akshikasharma1011@users.noreply.github.com>
2024-07-14 21:00:34 +0530

    Lecture 3.cpp

---
+++ Lecture 3.cpp
@@ -0,0 +1,17 @@
+In this lecture i learned about the Variables, Constants and Data Types .i mean what is the difference between the variables ,constants and data types.
+
+  [1.] Constants :- A constant is a fixed value that cannot change during the program's execution.
+             Think of it as a label attached to a specific value, like a price tag that stays the same.
+
+  [2.] Variables :- A variable is a named storage location in memory that holds a value.
+                    Unlike constants, the value stored in a variable can change throughout the program.
+                    Imagine a box where you can put things and take them out as needed. You can give the box a name (variable name) to easily refer to it.
+
+  [3.] Data Types :- A data type defines the type of data a variable can hold and the operations that can be performed on it.
+                     It's like having different sized containers for different things. A container for numbers wouldn't be suitable for storing text, and vice versa.
+                    Common data types include:
+                       Integers (whole numbers, like 10, -5)
+                       Floating-point numbers (numbers with decimals, like 3.14, -12.9)
+                       Characters (single letters or symbols, like 'a', '$')
+                       Strings (sequences of characters, like "Hello, world!")
+                       Booleans (logical values, True or False)
