07194c11d87742f21aeef633f7872a0693898d83
akshikasharma1011 <119088503+akshikasharma1011@users.noreply.github.com>
2024-07-14 20:48:52 +0530

    Create Lecture 0.cpp

---
+++ Lecture 0.cpp
@@ -0,0 +1,27 @@
+Ques1:- What is programming language?
+Ans:- A programming language is a computer language that is used by programmers (developers) to communicate with computers. It is a set of instructions written in any specific language ( C, C++, Java, Python) to perform a specific task.
+
+
+Ques2:- Why learn C++ instead of other Programming languages ?
+Ans:-
+1. Efficiency
+2. Strong abstraction capabilities
+3. Standard Template Library(STL)
+4. Best in resource constrained environment
+5. Strong typing and compile time checks
+
+
+Ques3:- What is an IDE ?
+Ans:- IDE stands for Integrated Development Environment, which is a software application that combines commonly used developer tools into a single graphical user interface (GUI). IDEs can include tools such as a code editor, code compiler, code debugger, and integrated terminal. They can also include features like software editing, building, testing, and packaging.
+
+
+Ques4:- What are high level language and low level language?
+Ans:- High level language :- These are programmer-friendly languages that are manageable, easy to understand, debug, and widely used in today’s times. High-level languages require the use of a compiler or an interpreter for their translation into the machine code.
+ 
+Low level language:- These are machine-friendly languages that are very difficult to understand by human beings but easy to interpret by machines.Low-level language requires an assembler for directly translating the instructions of the machine language.
+
+C++ is often considered an intermediate-level language because it bridges the gap between high-level and low-level programming.
+
+Ques5:- What is a compiler?
+
+Ans:- A compiler is a computer program that translates source code written in a high-level programming language into a low-level programming language or machine language to create an executable program
