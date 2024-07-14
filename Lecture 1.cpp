3c2dbe54d8a9d1d190dd77c80cea74b63a195fe6
akshikasharma1011 <119088503+akshikasharma1011@users.noreply.github.com>
2024-07-14 20:54:31 +0530

    Lecture 1.cpp

---
+++ Lecture 1.cpp
@@ -0,0 +1,6 @@
+In this lecture , 
+In this lecture , i learned about Introduction to C++ for learning the course of Data structures using C++ and he told that how about we patiance to the code.
+This the Zero lecture for the Course of Data Structures using C++.
+C++ is more efficiency ,strong abstraction capabilities ,standard Template Library(STL) and it is complie time checks . 
+There is no code introduction in this lecture so i didn't write. 
+so that's we use C++ other than programming languages.this all about lecture 1.
